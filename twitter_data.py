4a2a6c66c71e717ddc04c07367f6bb30d4685bc0
gcmurillo <gcmurillo@outlook.com>
2018-09-01 16:03:32 -0500

    initial twitter extract data

---
+++ twitter_data.py
@@ -0,0 +1,24 @@
+import tweepy
+
+consumer_key = "5VK8LjqeedkkhUCktDu4XsSd3"
+consumer_secret = "UylSTSKUEzmL9EyjdGPjwFai2LS0vD4y75smf24z7N5d8VeoIC"
+
+access_token = "227449172-UDe4SMobVp3JBTUvqjiAgK8IjlvM8FqK7monS0bV"
+access_token_secret = "1UVskkK5MQ5byfCDVYTUGgAbkqwoDrtvl2uUJKW2epIK7"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+public_tweets = api.home_timeline()
+# print(public_tweets)
+stuff = api.user_timeline(screen_name = 'luisevivanco', count = 5, include_rts = True)
+
+for status in stuff:
+    print(str(status._json).encode("utf-8"))
+
+"""
+for tweet in public_tweets:
+    print(str(tweet.keys()).encode("utf-8"))
+    print(api.me)
+"""
