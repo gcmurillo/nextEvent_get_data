414a0f7d0e251080acd3154b29abb7e98390458d
gcmurillo <gcmurillo@outlook.com>
2018-09-03 20:37:19 -0500

    getting tweets with hashtags

--- twitter_data.py
+++ twitter_data.py
@@ -12,13 +12,24 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 public_tweets = api.home_timeline()
 # print(public_tweets)
+"""
 stuff = api.user_timeline(screen_name = 'luisevivanco', count = 5, include_rts = True)
 
 for status in stuff:
     print(str(status._json).encode("utf-8"))
 
-"""
+
 for tweet in public_tweets:
     print(str(tweet.keys()).encode("utf-8"))
     print(api.me)
+"""
+print("CONCIERTOS")
+for tweet in tweepy.Cursor(api.search,q="#guayaquil #concierto",count=5, since="2018-08-01").items():
+    #print (tweet.created_at, tweet.text)
+    print(tweet.geo)
+
+"""
+print("\nTEATRO")
+for tweet in tweepy.Cursor(api.search,q="#guayaquil #teatro",count=100, since="2018-08-01").items():
+    #print (tweet.created_at, tweet.text)
 """
